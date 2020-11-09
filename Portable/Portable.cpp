f3619b937af1dbd4b0e999ae7991a9087767c79a
Haider <haideralhafiz@gmail.com>
2020-11-09 11:45:12 -0500

    Breaking user command into args

--- Portable/Portable.cpp
+++ Portable/Portable.cpp
@@ -2,24 +2,33 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <sstream>
+#include <iterator>
+#include <numeric>
+#include <algorithm>
 int main()
 {
    
 	std::string command;
-	std::string commandCpy;
+	std::string arg;
+	std::string delimiter = " ";
+	std::vector<std::string> listargs;
+	size_t pos = 0;
 	while (1)
 	{
 
 		std::cin >> command;
-		commandCpy = command;
-		std::string firstWord = command.substr(0, command.find(" "));
 		
-		if (firstWord == "create") {
-
-			std::cout << "You used create file command: " << commandCpy <<std::endl;
-
+		std::stringstream ss(command);
+		std::istream_iterator<std::string> begin(ss);
+		std::istream_iterator<std::string> end;
+		std::vector<std::string> vstrings(begin, end);
+		std::copy(vstrings.begin(), vstrings.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
+		
+		
+		if (vstrings.at(0) == "creat") {
+			std::cout << "create command" << std::endl;
 		}
-
 
 
 
