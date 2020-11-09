5b8c2ee8c1847167894c2713587472ab402389c8
aneelang <52489664+aneelang@users.noreply.github.com>
2020-11-10 00:08:53 +0530

    Update FileManipulator.h
    Deleting std scope

--- Portable/FileManipulator.h
+++ Portable/FileManipulator.h
@@ -2,14 +2,15 @@
 #include "vector"
 #include "string"
 
+
 constexpr auto COMMAND = 0 ;
 class FileManipulator {
 
 
 public:
-	static int createFile(std::vector<std::string> listArgs);
-	static int deleteFile(std::vector<std::string> listArgs);
-	static int renameFile(std::vector<std::string> listArgs);
+	static int createFile(vector<string> listArgs);
+	static int deleteFile(vector<string> listArgs);
+	static int renameFile(vector<string> listArgs);
 
 
 
